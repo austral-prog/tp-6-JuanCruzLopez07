e6f94267e8a4826e0036614f78feeb28563025ad
JuanCruzLopez07 <juancruzl2007@gmail.com>
2025-05-16 04:20:21 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -24,7 +24,7 @@ def is_empty(list_to_check):
 
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1) > 2 and len(list_to_compare2) > 2:
-        return lista1[2] == lista2[2]
+        return list_to_compare1[2] == list_to_compare2[2]
     else:
         return False
 
