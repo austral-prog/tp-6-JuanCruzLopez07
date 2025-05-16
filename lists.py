9e79879d6024622bee2951627aea7dd41c363c72
JuanCruzLopez07 <juancruzl2007@gmail.com>
2025-05-16 04:25:55 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -31,12 +31,15 @@ def check_lists(list_to_compare1, list_to_compare2):
 
 def list_of_lists(list_of_lists_to_modify):
     nueva_lista = []
+    if len(list_of_lists_to_modify) > 0:
         sublista1 = list_of_lists_to_modify[0][:2]
         nueva_lista.append(sublista1)
         
+    if len(list_of_lists_to_modify) > 1:
         sublista2 = list_of_lists_to_modify[1][1:4]
         nueva_lista.append(sublista2)
         
+    if len(list_of_lists_to_modify) > 2:
         sublista3 = list_of_lists_to_modify[2][-2:]
         nueva_lista.append(sublista3)       
     return nueva_lista
